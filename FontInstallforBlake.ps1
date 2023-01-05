71ce8642da88fd54f6f8323f2139b8165e7b11cd
timhayden8 <119264210+timhayden8@users.noreply.github.com>
2023-01-04 21:45:41 -0700

    Add files via upload

---
+++ FontInstallforBlake.ps1
@@ -0,0 +1,10 @@
+invoke-webrequest -uri "https://fonts.google.com/download?family=Urbanist" -outfile $psscriptroot\Urbanist.zip
+expand-archive -path $psscriptroot\urbanist.zip -destinationpath $psscriptroot\font
+$fonts = get-childitem $psscriptroot\font\static
+foreach ($font in $fonts)
+{
+   copy-item $font.FullName -destination "$($env:windir)\Fonts\ + $font.Name)" -force
+   Get-ItemProperty -Name $font.Name -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -ErrorAction SilentlyContinue   
+   New-ItemProperty -Name $font.Name -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $font.Name -Force -ErrorAction SilentlyContinue
+}
+read-host "press enter to exit"
